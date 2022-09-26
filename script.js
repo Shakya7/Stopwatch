208314b30694e4f8c606aaa02198f207c44ef7c7
Shakya7 <shakyasarkar72@gmail.com>
2022-09-27 01:13:41 +0530

    done stopwatch

--- script.js
+++ script.js
@@ -4,16 +4,27 @@ let sec=document.querySelector(".time-sec");
 let min=document.querySelector(".time-min");
 let hour=document.querySelector(".time-hr");
 
+let currentTiming=document.querySelector(".toggle");
+
 //DOM - Buttons
 let playBtn=document.querySelector(".btn.play");
 let pauseBtn=document.querySelector(".btn.pause");
 let resetBtn=document.querySelector(".btn.reset");
-let flagBtn=document.querySelector(".btn.laps");
+let flagBtn=document.querySelector(".btn.laps-btn");
+
+//DOM - Laps
+let lapsDiv=document.querySelector(".laps");
+let iconClassess=["fa-solid","fa-flag"];
 
 //Javascript logic variables
 let hours=0, mins=0, secs=0, miliSecs=0;
 let timer=false;
 
+//Laps variables
+let lapHr=0;
+let lapMin=0;
+let lapSec=0;
+let lapMSec=0;
 
 
 
@@ -74,6 +85,51 @@ function stopwatch(){
     }
 }
 
+function getExactTime(){
+    lapHr=hour.innerHTML;
+    lapMin= min.innerHTML;
+    lapSec=sec.innerHTML;
+    lapMSec=mSecs.innerHTML;
+
+    console.log(`${lapHr}:${lapMin}:${lapSec}:${lapMSec}`);
+
+    let newLap=document.createElement("div");
+    newLap.classList.add("lap");
+    let leftDiv=document.createElement("div");
+    leftDiv.classList.add("lap-left");
+
+    let numberDiv=document.createElement("p");
+
+    let iconSpan=document.createElement("i");
+    iconSpan.classList.add(...iconClassess);
+    leftDiv.appendChild(iconSpan);
+    leftDiv.appendChild(numberDiv);
+    
+    let timeDiv=document.createElement("p");
+    newLap.appendChild(leftDiv);
+    newLap.appendChild(timeDiv);
+    lapsDiv.prepend(newLap);
+    console.log(newLap.nextElementSibling);
+    if(newLap.nextElementSibling){
+        console.log(Number(newLap.nextElementSibling.firstChild.lastChild.innerHTML));
+        if(Number(newLap.nextElementSibling.firstChild.lastChild.innerHTML)<9)
+            numberDiv.innerHTML="0"+(Number(newLap.nextElementSibling.firstChild.lastChild.innerHTML)+1);
+        else
+            numberDiv.innerHTML=Number(newLap.nextElementSibling.firstChild.lastChild.innerHTML)+1;
+    }
+    else{
+        numberDiv.innerHTML="01";
+    }
+    timeDiv.innerHTML=`${lapHr}:${lapMin}:${lapSec}:${lapMSec}`;
+
+}
+
+function deleteLaps(parent){
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
 
 // Display appropriate buttons according to appropriate logic //
 
@@ -98,12 +154,18 @@ resetBtn.addEventListener("click",()=>{
     playBtn.classList.remove("hidden");
     flagBtn.classList.add("hidden");
     pauseBtn.classList.add("hidden");
+    lapsDiv.classList.add("hidden");
+    currentTiming.classList.add("hidden");
     reset();
+    deleteLaps(lapsDiv);
 
 })
 
 flagBtn.addEventListener("click",()=>{
-    console.log("Flag selected");
+    //console.log("Flag selected");
+    lapsDiv.classList.remove("hidden");
+    currentTiming.classList.remove("hidden");
+    getExactTime();
 })
 
 
